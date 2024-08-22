0e3b89403ddd73932ebad367d5127f1ccf94fe82
jescal93 <jasonescalera93@gmail.com>
2024-08-22 02:53:07 +0000

    js excercise

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Start')
+
+console.log('hello')
+
+console.log("animal")
+
+const myFavoriteAnimal = "dog"
+console.log(myFavoriteAnimal)
+
+const myName = "Jason"
+console.log(myName)
+
+let myAge = "31"
+console.log(myAge)
+
+let myLocation = "Phoenix"
+console.log(myLocation)
